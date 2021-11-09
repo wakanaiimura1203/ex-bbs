f00a577362840c97b87e745475c2ffa830b2509c
iimurawakana <wakana.iimura@rakus-partners.co.jp>
2021-11-09 17:59:17 +0900

    コメントをbbsに反映したい（途中）

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import java.util.List;
 
+import javax.xml.stream.events.Comment;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.example.domain.Article;
 import com.example.form.ArticleForm;
 import com.example.repository.ArticleRepository;
+import com.example.repository.CommentRepository;
 
 @Controller
 @RequestMapping("/bbs")
@@ -22,9 +24,15 @@ public class ArticleController {
 	public ArticleForm setUpForm() {
 		return new ArticleForm();
 	}
-	
+	/**
+	 * 各リポジトリ―を使う為の設定。
+	 */
 	@Autowired
 	private ArticleRepository articleRepository;
+	
+	@Autowired
+	private CommentRepository commentRepository;
+	
 	
 	/**
 	 * トップページを表示するメソッド
@@ -36,17 +44,15 @@ public class ArticleController {
 	}
 	
 	/**
-	 * 投稿された記事を反映する。
-	 *
+	 * 投稿された記事とコメントをbbsに反映する。
 	 */
 	@RequestMapping("/Reflect")
-	public String Reflect(Model model) {
+	public String reflect(Integer articleId,Model model) {
 		List<Article> articleList = articleRepository.findAll();
+		List<Comment> commentList = commentRepository.findByArticleId(articleId);
 		model.addAttribute("articleList",articleList);
+		model.addAttribute("commentList",commentList);
 		return "bbs";
 	}
-	
-
-	
 	
 }
--- src/main/java/com/example/repository/CommentRepository.java
+++ src/main/java/com/example/repository/CommentRepository.java
@@ -1,25 +1,45 @@
 package com.example.repository;
 
+import java.util.List;
+
 import javax.xml.stream.events.Comment;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CommentRepository {
 	
+	// SQLの実行処理をするために必要
 	@Autowired
 	private NamedParameterJdbcTemplate template;
 
 	/**
 	 * ラムダ式でローマッパーインターフェイスを実装。
-	 * リザルトセットから各要素を取得してhotelオブジェクトにセット。
+	 * リザルトセットから各要素を取得してcommentオブジェクト（ドメイン）にセット。
 	 * ※この時要素名データベースに合わせる。
 	 */
 	public static final RowMapper<Comment> COMMENT_ROW_MAPPER =
 			new BeanPropertyRowMapper<>(Comment.class);
+	
+	/**
+	 * 記事idから各記事に対するコメントを取得。
+	 * findByArticleIdメソッドが呼ばれたときにcommentListを返す。
+	 * @param 記事id
+	 * @return 各記事idに対応するコメント
+	 * 
+	 */
+	public List<Comment> findByArticleId(Integer articleId){
+		String sql = "SELECT * FROM comments WHERE article_id=:article_id;";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("article_id",articleId);
+		
+		List<Comment> commentList = template.query(sql,param,COMMENT_ROW_MAPPER);
+		return commentList;
+	}
 
 }
